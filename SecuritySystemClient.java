9af1babfc87cebd3e79e94897537a06932d79540
Elena Cina <cinaelena15@gmail.com>
2018-03-13 22:00:22 +0300

    Update SecuritySystemClient.java

--- SecuritySystemClient.java
+++ SecuritySystemClient.java
@@ -3,138 +3,139 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.lang.*;
+
 
 public class SecuritySystemClient {
-    public static void main( String args[]) throws IOException {
-        int i = 0;
-
-        int port = 1234;// Integer.parseInt(args[0]);
+    public static void main(String args[]) throws IOException {
+        int snapShotNumber = 1;
+        int port = Integer.parseInt(args[0]);
         int type;
-        String username = "bilkent";//args[1];
-        String password  = "cs421";//args[2];
+        String username = args[1];
+        String password = args[2];
         String data = username + ":" + password;
         boolean exit = false;
-        try {
-            Socket client = new Socket("localhost",port );
 
-            //the output stream is connected to the input stream
-            // OutputStream outToServer = client.getOutputStream();
+        Socket client = new Socket("localhost", port);
 
-            //send the authentication message
-            DataOutputStream outToServer = new DataOutputStream(client.getOutputStream());
+        DataOutputStream outToServer = new DataOutputStream(client.getOutputStream());
+        DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(client.getInputStream()));//data coming from server
 
-            //first we send the first byte which in case of authorize is 0
-            outToServer.writeByte(0);
-            //then we send data size
+        //first we send the first byte which in case of authorize is 0
+        outToServer.writeByte(0);
 
-            //now we pass the actual date which is username and password
-            //first convert them to US-ASCII standard
-            byte [] us =  data.getBytes(StandardCharsets.US_ASCII);
-            short length = (short) data.length();
-            outToServer.writeShort(length);
-            outToServer.write(us); //data send
 
-            //getting response from server
-            DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(client.getInputStream()));//data coming from server
-            type = inFromServer.readByte();
-//            System.out.println("Type recieved is :" + type);
+        //now we pass the actual date which is username and password
+        //first convert them to US-ASCII standard
+        byte[] us = data.getBytes(StandardCharsets.US_ASCII);
+        int length = data.length();
+        outToServer.writeShort(length); // data size
+        outToServer.write(us); //data send
+        outToServer.flush();
 
-            length = inFromServer.readShort();
-//            System.out.println("Length is " + length);
+        //getting response from server
+        type = inFromServer.readByte();
+        length = inFromServer.readShort() & 0xffff ; // maskinbg the length to make the integer short type
 
-            if(type == 2)
+        if (type == 2) {
+            System.out.println("Authenication Successfull");
+            while (exit == false)
             {
+                type = inFromServer.readByte();
+                length = inFromServer.readShort() & 0xffff;
 
-                System.out.println("Authenication Successfull");
-                while(exit == false)
-               {
-                    type = inFromServer.readByte();
-                    System.out.println("Type is " + type );
-                    length = inFromServer.readShort();
-                    System.out.println("Length is " + length);
+                if (type == 1) {
+                    int type2;
+                    //sending keep alive message back to the server
+                    byte sendType = 1;
+                    short sendLen = 0;
+                    outToServer.writeByte(sendType);
 
-                    if(type == 1)
+                    try { Thread.sleep(100);}
+                    catch(Exception e){}
+                    outToServer.writeShort(sendLen);
+
+                    outToServer.flush(); //remove previous write value
+
+                    //waitiing for the server to send client
+                    type2 = inFromServer.readByte();
+                    length = inFromServer.readShort() & 0xffff;
+
+                    if (type2 == 2)
                     {
-                        System.out.println("Recieved KeepAlive Signal ");
-                        //sending keep alive message back to the server
-                        outToServer.writeByte(1);
+                        System.out.println("Recieved OK");
+                    }
+                    else if (type2 == 3)
+                    {
+                        System.out.println("Invalid");
+                    }
+
+                } else if (type == 4) {
+                    int type2;
+
+                    byte[] screenShot = new byte[length];
+                    inFromServer.read(screenShot , 0 , length);
+
+                    String snapShotName = "snapshot_" + snapShotNumber + ".txt";
+                    snapShotNumber++;
+
+                    BufferedWriter outputWriter = new BufferedWriter(new FileWriter(snapShotName));
+                    for(int i = 0 ; i < length ; i++) {
+                        outputWriter.write(screenShot[i]);
+                    }
+                    outputWriter.flush();
+
+                    Scanner reader = new Scanner(System.in);  // Reading from System.in
+                    System.out.println("\n----------------------\nEmergency message received. Enter 1 to ring the alarm, enter 2 to discard: ");
+                    int input = reader.nextInt();
+                    if (input ==1){//if the user entered 1 -->ALARM MESSAGE
+                        outToServer.writeByte(5);
                         outToServer.writeShort(0);
+                        System.out.println("Alarm message sent.");
 
-                        //waitiing for the server ot send me back the shitty
-                        type = inFromServer.readByte();
-                        length = inFromServer.readShort();
-                        if(type == 2)
+                        outToServer.flush();
+
+                        type2 = inFromServer.readByte();
+                        length = inFromServer.readShort() & 0xffff;
+                        if(type2 == 2)
                         {
-                            System.out.println("Recieved OK\n-----------");
-
+                            System.out.println("Recieved OK");
                         }
-                        else  if(type == 3)
+                        else  if(type2 == 3)
                         {
-                            System.out.println("Invalid");
-
+                            System.out.println("Invalid sending alarm message");
                         }
-
                     }
-                    else if (type == 4){
-                        Scanner reader = new Scanner(System.in);  // Reading from System.in
-                        System.out.println("Emergency message received. Enter 1 to ring the alarm, enter 2 to discard: ");
-                        int input = reader.nextInt();
-                        if (input ==1){//if the user entered 1 -->ALARM MESSAGE
-                            outToServer.writeByte(5);
-                            outToServer.writeShort(0);
-                            System.out.println("Alarm message sent.");
+                    else if (input == 2) {//if user enters 2 --> DISCARD MESSAGE
+                        outToServer.writeByte(6);
+                        outToServer.writeShort(0);
+                        System.out.println("Discard message sent.");
 
-                            type = inFromServer.readByte();
-                            if(type == 2)
-                            {
-                                System.out.println("Recieved OK for sent alarm message \n-----------");
+                        outToServer.flush();
 
-                            }
-                            else  if(type == 3)
-                            {
-                                System.out.println("Invalid sending alarm message");
-
-                            }
+                        type2 = inFromServer.readByte();
+                        length = inFromServer.readShort() & 0xffff;
+                        if(type2 == 2)
+                        {
+                            System.out.println("Recieved OK");
                         }
-                        else if (input == 2) {//if user enters 2 --> DISCARD MESSAGE
-                            outToServer.writeByte(6);
-                            outToServer.writeShort(0);
-                            System.out.println("Discard message sent.");
-
-                            type = inFromServer.readByte();
-                            if(type == 2)
-                            {
-                                System.out.println("Recieved OK for sent discard message \n-----------");
-
-                            }
-                            else  if(type == 3)
-                            {
-                                System.out.println("Invalid sending discard message");
-
-                            }
+                        else if(type2 == 3)
+                        {
+                            System.out.println("Invalid sending discard message");
                         }
-                        System.out.println("in 4444");
-                    }
-                    else if (type == 7){
-                        System.out.println ("------- exit is : " + exit);
-                        System.out.println("EXITING NOW");
-                        exit = true;
-                     //   client.close();
-                    }
-                    else{
-                        System.out.println("Nothing recieved");
                     }
                 }
+                else if (type == 7) {
+                    System.out.println("EXITING NOW");
+                    exit = true;
+                }
+                else {
+                }
             }
-            else if(type == 3) {
-                System.out.println("Invalid");
-            }
-            else{
-                System.out.println("not 1 not 3");
-            }
-
-       }catch (IOException e){
-           e.printStackTrace();
+        } else if (type == 3) {
+            System.out.println("Invalid");
         }
+
     }
+
 }
